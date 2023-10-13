3a9a4876c1ee019b10d7d06526f0ff87a9f07c32
yunshin <2younsin@gmail.com>
2023-10-14 00:04:15 +0900

    🌀 Chore: 나홀로 컨벤션 적용

--- yunshin/2주차/최종_자바스크립트_문제풀이하기_(Lv1~Lv3)/level1/level.js
+++ yunshin/2주차/최종_자바스크립트_문제풀이하기_(Lv1~Lv3)/level1/level.js
@@ -1,67 +1,58 @@
 import { BANK_LIST } from './account.js'
 
-/* 
-    - dom 접근 객체 
-        => "$(class 혹은 id 속성값)$(속성)$(dom 요소)"
-        
-    - querySelect() 또는 querySelectAll() 로 가져오기   
-        선택자 형식 
-            => '(dom 요소), (class 혹은 id 속성값)'
-*/
-
 // 페이지 로딩 직후, 드롭박스에 은행이름 넣기
 document.addEventListener('DOMContentLoaded', () => {
-  for (const bankName of Object.values(BANK_LIST)) {
-    InsertOption(bankName)
+  for (const bank_name of Object.values(BANK_LIST)) {
+    InsertOption(bank_name)
   }
 })
-const $account_input$id$input = document.querySelector('input, #account-input')
-$account_input$id$input.addEventListener('keypress', (e) => {
+const $input$id$account_input = document.querySelector('input#account-input')
+$input$id$account_input.addEventListener('keypress', (e) => {
   if (e.key === 'Enter') {
-    const writtenText = e.target.value
+    const text = e.target.value
 
-    if (writtenText.length < 12) {
+    if (text.length < 12) {
       alert('12자리가 아닙니다.')
       e.preventDefault()
       return
     }
-    const parsedAccount = ParseAccount(writtenText)
-    const completedText = AppendBankName(parsedAccount)
-    SummitAccount(completedText)
+    const parsed_account = ParseAccount(text)
+    const completed_text = AppendBankName(parsed_account)
+    SummitAccount(completed_text)
     e.preventDefault()
   }
 })
 // 전달된 계좌 번호에 은행이름 붙혀주기
 const AppendBankName = (account) => {
-  const $back_selector$id$select = document.querySelector(
-    'select, #bank-selector'
+  const $select$id$back_selector = document.querySelector(
+    'select#bank-selector'
   )
-  return $back_selector$id$select.value + ' : ' + account
+  return $select$id$back_selector.value + ' : ' + account
 }
 // 전달된 계좌번호 중간부분 가려주기
 const ParseAccount = (account) => {
-  const rawAccountArray = account.split('')
-  const parsedAccountArray = rawAccountArray.map((num, idx) => {
+  const raw_account_array = account.split('')
+  const parsed_account_array = raw_account_array.map((num, idx) => {
     if (idx === 2) return '-*'
     if (2 < idx && idx < 9) return '*'
     if (idx === 9) return '*-'
     return num
   })
-  return parsedAccountArray.join('')
+  return parsed_account_array.join('')
 }
 // 전달된 Text를 li 태그로 감싸서 올리기
-const SummitAccount = (insertText) => {
-  const $account_list$id$ul = document.querySelector('ul, #account-list')
+const SummitAccount = (insert_text) => {
+  const $ul$id$account_list = document.querySelector('ul#account-list')
   const $li = document.createElement('li')
-  $li.innerText = insertText
-  $account_list$id$ul.appendChild($li)
+  $li.innerText = insert_text
+  $ul$id$account_list.appendChild($li)
 }
 // 전달받은 Text를 option 태그로 감싸서 select 항목으로 만들기
-const InsertOption = (insertText) => {
-  const $back_selector$id$select = document.querySelector(
-    'select, #bank-selector'
+const InsertOption = (insert_text) => {
+  const $select$id$back_selector = document.querySelector(
+    'select#bank-selector'
   )
   const $option = document.createElement('option')
-  $option.innerText = insertText
-  $back_selector$id$select.appendChild($option)
+  $option.innerText = insert_text
+  $select$id$back_selector.appendChild($option)
 }
