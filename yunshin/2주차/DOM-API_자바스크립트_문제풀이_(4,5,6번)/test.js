0e4ae377900070343646477c26ac5f8859c5f64e
yunshin <2younsin@gmail.com>
2023-10-13 12:51:13 +0900

    ✨ Feat : 2주차 DOM-API 6번 문제 수정기능 추가

--- yunshin/2주차/DOM-API_자바스크립트_문제풀이_(4,5,6번)/test.js
+++ yunshin/2주차/DOM-API_자바스크립트_문제풀이_(4,5,6번)/test.js
@@ -34,14 +34,17 @@ $저장_button.addEventListener('click', (e) => {
 
 // 3. 초기화 버튼을 누른 경우
 $초기화_button.addEventListener('click', (e) => {
-  const input_적힌_text = $두번째_input.value
+  ul_있는거_다_없애기()
+  e.preventDefault() // 페이지 새로고침 막기
+})
+
+const ul_있는거_다_없애기 = () => {
   while ($id가_list인_ul.childElementCount) {
     // ul의 자식 요소 갯수가 0이 아닐동안
     const ul의_마지막자식 = $id가_list인_ul.lastChild // ul 의 자식 중 마지막 요소부터
     $id가_list인_ul.removeChild(ul의_마지막자식) // 하나씩 제거
   }
-  e.preventDefault() // 페이지 새로고침 막기
-})
+}
 
 const 새로운_리스트_요소_만들기 = (적을내용) => {
   const $new_li = document.createElement('li')
@@ -69,25 +72,86 @@ const 새로운_리스트_요소_만들기 = (적을내용) => {
 }
 
 /* [6] */
+
+// 1. 삭제
 /**
- * 주의!
+ * 이벤트 위임
  *
  * 동적으로 요소를 추가한 경우, querySelector 로 직접 접근불가
  * 정적인 상위 부모 요소로부터 원하는 요소로 접근해야한다.
- *
- * => 이벤트 위임
  */
-
 $id가_list인_ul.addEventListener('click', (e) => {
   if (e.target.classList.contains('delete_btn')) {
     const 삭제_버튼_모임 = Array.from(
       $id가_list인_ul.querySelectorAll('.delete_btn')
     )
     const 지워야할_요소의_idx = 삭제_버튼_모임.indexOf(e.target)
+    idx_주면_list요소_지워드림(지워야할_요소의_idx)
+  }
+})
+
+const idx_주면_list요소_지워드림 = (idx) => {
+  const liclass_모임 = Array.from($id가_list인_ul.querySelectorAll('.liclass'))
+  liclass_모임[idx].remove()
+}
+
+// 2. 수정
+$id가_list인_ul.addEventListener('click', (e) => {
+  if (e.target.classList.contains('edit_btn')) {
+    const 수정_버튼_모임 = Array.from(
+      $id가_list인_ul.querySelectorAll('.edit_btn')
+    )
+    const 수정할_요소의_idx = 수정_버튼_모임.indexOf(e.target)
+    console.log(수정할_요소의_idx)
+
+    const $수정할_요소의_li_속_p = Array.from(
+      $id가_list인_ul.querySelectorAll('.liclass > p')
+    )[수정할_요소의_idx]
+
+    const hasInputTag = $수정할_요소의_li_속_p.querySelector('input')
+
+    if (hasInputTag) {
+      hasInputTag.remove()
+    } else {
+      $new_input = document.createElement('input')
+      $new_input.classList.add('new_save_input')
+      $new_새로저장_button = document.createElement('button')
+      $new_새로저장_button.classList.add('new_save_btn')
+      $new_새로저장_button.type = 'button'
+      $new_새로저장_button.innerHTML = '새로저장'
+
+      $수정할_요소의_li_속_p.appendChild($new_input)
+      $수정할_요소의_li_속_p.appendChild($new_새로저장_button)
+    }
+  }
+})
+
+$id가_list인_ul.addEventListener('click', (e) => {
+  if (e.target.classList.contains('new_save_btn')) {
+    const 새로저장_버튼 = e.target
+    const 부모_liclass = 새로저장_버튼.closest('.liclass')
+    const 부모_liclass_idx = Array.from(
+      $id가_list인_ul.querySelectorAll('.liclass')
+    ).indexOf(부모_liclass)
+
+    const 변경_text = 부모_liclass.querySelector('input').value
 
     const liclass_모임 = Array.from(
       $id가_list인_ul.querySelectorAll('.liclass')
     )
-    liclass_모임[지워야할_요소의_idx].remove()
+
+    ul_있는거_다_없애기()
+
+    for (let i = 0; i < 부모_liclass_idx; i++) {
+      새로운_리스트_요소_만들기(
+        liclass_모임[i].querySelector('p').textContent.split('수정')[0].trim()
+      )
+    }
+    새로운_리스트_요소_만들기(변경_text)
+    for (let i = 부모_liclass_idx + 1; i < liclass_모임.length; i++) {
+      새로운_리스트_요소_만들기(
+        liclass_모임[i].querySelector('p').textContent.split('수정')[0].trim()
+      )
+    }
   }
 })
