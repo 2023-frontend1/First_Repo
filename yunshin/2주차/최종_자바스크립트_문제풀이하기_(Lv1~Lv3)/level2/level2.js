69fc2c1fd0d87b9d4b16810b5eccc6d5bea823ce
yunshin <2younsin@gmail.com>
2023-10-14 10:42:32 +0900

    ♻️ Refactor : input이 비어있더라도, 기능이 구현되도록 코드 개선

--- yunshin/2주차/최종_자바스크립트_문제풀이하기_(Lv1~Lv3)/level2/level2.js
+++ yunshin/2주차/최종_자바스크립트_문제풀이하기_(Lv1~Lv3)/level2/level2.js
@@ -1,55 +1,40 @@
 import { RESERVATION_LIST } from './reservation .js'
-console.log(RESERVATION_LIST)
 
 /* 예약 고객확인하기 */
 
-const $input$name$user_name = document.querySelector('input[name=user-name]')
-const $input$name$user_phone = document.querySelector('input[name=user-phone]')
 const $button = document.querySelector('button')
 
-const input_cached_data = {
-  name: '',
-  phone: '',
-}
-
-// 이름 입력 관련하여, 이벤트 처리
-$input$name$user_name.addEventListener('keypress', (e) => {
-  if (e.key === 'Enter') {
-    // input 값을 input_cached_data 객체의 name 에 저장
-    input_cached_data.name = e.target.value
-    e.preventDefault()
-  }
-})
-// 전화번호 입력 관련하여, 이벤트 처리
-$input$name$user_phone.addEventListener('keypress', (e) => {
-  // 이름이 입력되어 있지 않다면, alert 후 early return
-  if (input_cached_data.name.length === 0) {
-    alert("이름 먼저 입력 후, ⛔️ 'Enter' 를 꼭 입력해주세요!")
-    e.preventDefault()
-    return
-  }
-  if (e.key === 'Enter') {
-    input_cached_data.phone = e.target.value
-    e.preventDefault()
-  }
-})
 // 버튼 클릭 시, 이벤트 처리
 $button.addEventListener('click', (e) => {
-  // 전화번호가 입력되어 있지 않다면, alert 후 early return
-  if (input_cached_data.phone.length === 0) {
-    alert("전화번호 먼저 입력 후, ⛔️ 'Enter' 를 꼭 입력해주세요!")
-    e.preventDefault()
-    return
-  }
-  // RESERVATION_LIST 를 순회하며, name 과 phone 이 같으면, 예약번호 출력
+  const input_name = GetUserName()
+  const input_phone = GetUserPhone()
+
+  let reservation_number = -1
   for (const { name, phone, number } of RESERVATION_LIST) {
-    if (input_cached_data.name === name && input_cached_data.phone === phone) {
-      PrintReservationNumber(number)
-    }
+    if (input_name === name && input_phone === phone)
+      reservation_number = number
+    break
   }
+  // 예약 번호가 없으면
+  reservation_number === -1
+    ? alert('일치하는 내역이 없습니다.') // 경고창
+    : PrintReservationNumber(reservation_number) // 있으면, 출력
+
   e.preventDefault()
 })
 
+// 이름 관련 input 의 값을 가져온다.
+const GetUserName = () => {
+  const $input$name$user_name = document.querySelector('input[name=user-name]')
+  return $input$name$user_name.value
+}
+// 전화번호 관련 input 의 값을 가져온다.
+const GetUserPhone = () => {
+  const $input$name$user_phone = document.querySelector(
+    'input[name=user-phone]'
+  )
+  return $input$name$user_phone.value
+}
 // 예약 번호를 p 태그 안에 출력
 const PrintReservationNumber = (number) => {
   const $p$id$reservation_number = document.querySelector(
