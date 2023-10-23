627ad10cfaa39cf7c6dd7abaf28c9ad1e6a3c15c
seungwonyu <y123587979@gmail.com>
2023-10-23 21:02:49 +0900

    ✨ Feat : [복습 DOM 1번문제 ] 기능 추가

--- seungwon/4주차/특별플로우/복습/dom/dom.js
+++ seungwon/4주차/특별플로우/복습/dom/dom.js
@@ -1,31 +1,32 @@
 const users = [
-  {
-    id: 1,
-    name: '김사과',
-    age: 20,
-    height: 190,
-  },
-  {
-    id: 2,
-    name: '이수박',
-    age: 32,
-    height: 185,
-  },
-  {
-    id: 3,
-    name: '오렌지',
-    age: 20,
-    height: 180,
-  },
-  {
-    id: 4,
-    name: '이멜론',
-    age: 28,
-    height: 175,
-  },
+	{
+		id: 1,
+		name: "김사과",
+		age: 20,
+		height: 190,
+	},
+	{
+		id: 2,
+		name: "이수박",
+		age: 32,
+		height: 185,
+	},
+	{
+		id: 3,
+		name: "오렌지",
+		age: 20,
+		height: 180,
+	},
+	{
+		id: 4,
+		name: "이멜론",
+		age: 28,
+		height: 175,
+	},
 ];
 
-const $info = document.querySelector('#info');
+const $info = document.querySelector("#info");
+const buttonArray = document.querySelectorAll("button"); //버튼들 다 받아와서 배열로
 $info.innerHTML = `
     <div>${users[0].name}</div>
     <div>${users[0].age}</div>
@@ -39,3 +40,25 @@ $info.innerHTML = `
 
 또한 DOM API와 배열을 다뤄야할 때 인덱스가 필요한 시점이나 상황이 언제일지 고민해보세요 :)
 */
+
+let Index = 0;
+//인덱스 돌릴부분 미리 선언
+
+const PreButton = buttonArray[0]; // 이전 버튼 설정
+PreButton.addEventListener("click", () => {
+	Index = (Index - 1 + users.length) % users.length;
+	UserInfo();
+});
+
+const nexButton = buttonArray[1];
+nexButton.addEventListener("click", () => {
+	Index = (Index + 1) % users.length;
+	UserInfo();
+});
+
+function UserInfo() {
+	$info.innerHTML = `
+  <div>${users[Index].name}</div>
+  <div>${users[Index].age}</div>
+  <div>${users[Index].height}</div>`;
+}
