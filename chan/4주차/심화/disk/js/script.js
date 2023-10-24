55b9c4c7d86aaacd95130bd158f98668414e30b5
snkchan <qkrcodbs122@gmail.com>
2023-10-25 00:28:11 +0900

    ✨ Feat: next,prev버튼 클릭시 배경화면 색상변경 기능 구현

--- chan/4주차/심화/disk/js/script.js
+++ chan/4주차/심화/disk/js/script.js
@@ -27,26 +27,22 @@ const renderMusicList = () => {
 window.onload = renderMusicList();
 //musicListData를 순회하며 src를 <img>에넣어주면서 ul태그의 자식들로 넣어주며 rendering
 //---------------------------------------------------------------------------
-//$albumcoverList에 img정보를 모두 담는다.
 const $albumcover_List = Array.from(
   document.querySelectorAll(".list_btn_group > ul > img")
 );
-
-//앨범커버가 선택됬을때
-//1. 커버이미지 확대  2. 배경화면이 커버이미지로 바뀜
-
+const $main = document.querySelector('main')
+const $filter = document.querySelector('.filter')
 // 커버이미지 선택시 배경화면 바꿔주는 함수
 const changeBackgroundcolor = (index) => {
-  $filter.classList.add("main");
-  
-};
-//선택된 앨범 커버확인하는 함수
+  $main.style.background = `linear-gradient(120deg, ${musicListData[index].color[0]},  ${musicListData[index].color[1]})`
+}
+//선택된 앨범 커버확인하는 함수 
 const clickAlbumCover = (img) => {
   if (img.target.classList.contains("album_cover")) {
     $albumcover_List.forEach((el, i) => {
       if (el.contains(img.target)) {
         $albumcover_List[i].classList.add("play");
-        // changeBackgroundcolor(i) 
+        changeBackgroundcolor(i)
       } else {
         $albumcover_List[i].classList.remove("play");
       }
@@ -56,16 +52,14 @@ const clickAlbumCover = (img) => {
 //클릭된 앨범커버 사진 확대해주는 event
 $imageList.addEventListener("click", clickAlbumCover);
 //---------------------------------------------------------------------------
-//2.버튼들을가져와서 이벤트부터 만들어주자
 const $playBtn = document.querySelector(".play_btn");
 const $stopBtn = document.querySelector(".stop_btn");
 const $nextBtn = document.querySelector(".next_btn");
 const $prevBtn = document.querySelector(".prev_btn");
 //---------------------------------------------------------------------------
-// $nextBtn.addEventListener("click",f );
-//==> 버튼을 클릭하면 -> 현재 click클래스는 remove하고 -1번째 앨범컵버의 click on
+//prevBtn click event 
 $prevBtn.addEventListener("click", (button) => {
-  let curIdx ;
+  let curIdx;
   $albumcover_List.forEach((img, i) => {
     if (img.classList.contains("play")) {
       img.classList.remove("play");
@@ -75,8 +69,10 @@ $prevBtn.addEventListener("click", (button) => {
   if (curIdx == 0) {
     curIdx = $albumcover_List.length;
   }
+  changeBackgroundcolor(curIdx-1)
   $albumcover_List[curIdx - 1].classList.add("play");
 });
+//nextBtn click event 
 $nextBtn.addEventListener("click", (button) => {
   let curIdx;
   $albumcover_List.forEach((img, i) => {
@@ -85,15 +81,14 @@ $nextBtn.addEventListener("click", (button) => {
       curIdx = i;
     }
   });
-  if (curIdx == ($albumcover_List.length-1)) {
-    curIdx=-1
+  if (curIdx == $albumcover_List.length - 1) {
+    curIdx = -1;
   }
+  changeBackgroundcolor(curIdx+1)
   $albumcover_List[curIdx + 1].classList.add("play");
 });
-// $prevBtn.addEventListener("click",f );
 //---------------------------------------------------------------------------
-//배경화면
-const $filter = document.querySelector(".filter");
+
 //play버튼을 누르면 현재 커버이미지를 바탕화면으로 보여주는 함수
 const changeBackgroundImg = (index) => {
   $filter.classList.add("main");
