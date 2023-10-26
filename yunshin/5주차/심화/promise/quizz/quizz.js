4224d169db318f778a427ad30798543d340c5727
yunshin <2younsin@gmail.com>
2023-10-26 22:44:00 +0900

    ✨ Feat: quizz, async/await 로 문제풀이 추가

--- yunshin/5주차/심화/promise/quizz/quizz.js
+++ yunshin/5주차/심화/promise/quizz/quizz.js
@@ -1,21 +1,40 @@
 const 기초 = () => {
-  console.log('min size')
-  const promise = new Promise((resolve, reject) => {
-    let data = 1
+  const promise = new Promise((resolve, reject) =>
     setTimeout(() => {
+      let data = 1
       if (data || data === 0) resolve(data)
       reject('NO_DATA')
     }, 700)
-  })
+  )
+  console.log('min size')
   promise
     .then((val) => {
       console.log('...0.7초 후')
       console.log('max size')
       console.log(`이번주 목표까지 ${val}% 남았습니다`)
     })
-    .catch(() => {
-      console.log('이번주 목표를 모르겠는데요?? ㅎㅎ')
+    .catch((e) => {
+      console.log(e)
     })
 }
 
-기초()
+const 심화 = async () => {
+  console.log('min size')
+  try {
+    let val = await new Promise((resolve) =>
+      setTimeout(() => {
+        let data = 1
+        resolve(data)
+      }, 700)
+    )
+    if (val !== 0 && !val) throw Error('NO_DATA')
+    console.log('...0.7초 후')
+    console.log('max size')
+    console.log(`이번주 목표까지 ${val}% 남았습니다`)
+  } catch (e) {
+    console.log(e.message)
+  }
+}
+
+// 기초()
+심화()
