c102b0c62bcdea590d899d9f0a7685a18373fdf4
YooJin Kim <lily@YooJins-Laptop.local>
2023-10-29 20:55:43 +0900

    ✨ Feat: 03.js 수정

--- yoojin/4주차/JS_Checklist/3~4/03.js
+++ yoojin/4주차/JS_Checklist/3~4/03.js
@@ -4,6 +4,7 @@
 (1) 자바스크립트에서 분기점을 구현하기 위해서는 어떠한 실행문을 응용할 수 있을까요?
         => if 문
         => switch 문 
+
 (2) 자바스크립트에서 분기점이 생성되는 상황은 언제일까요?
 	if-else is best used when there are two discrete values or a few different ranges of values for which to test. 
     When there are more than two discrete values for which to test, the switch statement is the most optimal choice.
@@ -18,7 +19,6 @@ function ifTest(b){
     if (b < 6){
         console.log('b is less than 6')
     } else {
-        b > 6
         console.log('b is equal or greater than 6')}
 
     return;
@@ -28,22 +28,23 @@ ifTest(7);
 
 //switch문 예시
 
-const switchTest = 'example'
+// const switchTest = 'example'
 
-switch(switchTest){
- case 'test': 
-    console.log('this case says "test"')
-    break;
- case 'example':
-    console.log('this case says "example')
-    break;
- case 'TEST' :
-    console.log('this case says "TEST"')
-    break;
- case 'Hi' :
-    console.log('Hello')
-    break;
- case 'How are you today?' :
-    console.log('I dont know')
- default : console.log('Well.. you tried :) ')
-}
+// switch(switchTest){
+//  case 'test': 
+//     console.log('this case says "test"')
+//     break;
+//  case 'example':
+//     console.log('this case says "example')
+//     break;
+//  case 'TEST' :
+//     console.log('this case says "TEST"')
+//     break;
+//  case 'Hi' :
+//     console.log('Hello')
+//     break;
+//  case 'How are you today?' :
+//     console.log('I dont know')
+
+//  default : console.log('Well.. you tried :)')
+// }
