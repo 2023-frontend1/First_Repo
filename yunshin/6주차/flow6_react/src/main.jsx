f6d2456f28b31dcc8e6119229fa31e004df3d124
yunshin <2younsin@gmail.com>
2023-11-02 00:45:47 +0900

    ✨ Feat: router 설정

--- yunshin/6주차/flow6_react/src/main.jsx
+++ yunshin/6주차/flow6_react/src/main.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import * as React from 'react'
+import * as ReactDOM from 'react-dom/client'
+import { RouterProvider } from 'react-router-dom'
 import './index.css'
+// import Root from '/src/routes/root'
+import router from './router.jsx'
+
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <Root />,
+//   },
+// ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <RouterProvider router={router} />
+  </React.StrictMode>
 )
