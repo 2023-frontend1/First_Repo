59df455bcfbd0de06e45ea251f3674da61f943e7
Lily0nline <awkwardblogger3@gmail.com>
2023-11-04 00:11:20 +0900

    ✨ Feat: 메인 route 페이지 추가

--- yoojin/React 2주차/flow_6th_react/src/main.jsx
+++ yoojin/React 2주차/flow_6th_react/src/main.jsx
@@ -6,12 +6,40 @@ import {
 } from "react-router-dom";
 import "./index.css";
 import Root from "./routes/root";
+import Button_page from "./React flow pages/react_flow_page02/Button_page";
+import Number_page from "./React flow pages/react_flow_page02/Number_page";
+import MainPage from "./MainPage";
+import Q_and_A from "./React flow pages/react_flow_page01/Q&A";
+import UseState from "./React flow pages/react_flow_page04/UseState";
+import Example from "./React flow pages/react_flow_page04/Useref";
+import Useref from "./React flow pages/react_flow_page04/Useref";
+
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <Root/>
+    path: "",
+    element: <MainPage/>
   },
+  {
+    path: "react_flow_02_Btn_page",
+    element: <Button_page/>
+  },
+  {
+    path: ":number",
+    element: <Number_page/>
+  },
+  {
+    path: "react_flow_01_qna_page",
+    element: <Q_and_A/>
+  },
+  // {
+  //   path: "react_flow_04_page",
+  //   element: <UseState />
+  // },
+  {
+    path: "react_flow_04_page",
+    element: <Useref/>
+  }
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
